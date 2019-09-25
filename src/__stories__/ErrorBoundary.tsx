17a1a9e09786fbed1b78e8e2fc42c6142310d92b
Jakub Rożek <jakub@stoplight.io>
2019-09-26 01:10:42 +0200

    chore: add stories

---
+++ src/__stories__/ErrorBoundary.tsx
@@ -0,0 +1,35 @@
+import { action } from '@storybook/addon-actions';
+import { object, text, withKnobs } from '@storybook/addon-knobs';
+import { storiesOf } from '@storybook/react';
+import * as React from 'react';
+
+import { ErrorBoundary, withErrorBoundary } from '../';
+
+const SchemaViewer: React.FunctionComponent<{ name: string; schema: unknown }> = ({ schema, name }) => {
+  if (typeof schema !== 'object' || schema === null) {
+    throw new Error('Schema must be an object');
+  }
+
+  if (Object.keys(schema).length === 0) {
+    throw new Error('Schema cannot be empty');
+  }
+
+  return <span>This is fine.</span>;
+};
+
+const MyWrappedComponent = withErrorBoundary(SchemaViewer, {
+  recoverableProps: ['schema'],
+  onError: action('onError'),
+});
+
+const CustomFallbackComponent = () => <div>Errored.</div>;
+
+storiesOf('ErrorBoundary', module)
+  .addDecorator(withKnobs)
+  .add('default', () => <MyWrappedComponent schema={null} name={text('name', '')} />)
+  .add('recovering', () => <MyWrappedComponent schema={object('schema', {})} name={text('name', '')} />)
+  .add('with custom fallback', () => (
+    <ErrorBoundary FallbackComponent={CustomFallbackComponent}>
+      <SchemaViewer name="" schema={null} />
+    </ErrorBoundary>
+  ));
